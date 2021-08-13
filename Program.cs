9497c3ab1c56f345469b7a97b970fdbf190bda46
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-08-13 13:16:35 -0400

    Updated Readme, Classes and Methods

--- Program.cs
+++ Program.cs
@@ -2,27 +2,111 @@
 
 namespace RhythmsGonnaGetYou
 {
-    class Program
+
+    class Bands
+
     {
-        static void Main(string[] args)
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public int NumberOfMembers { get; set; }
+        public string Website { get; set; }
+        public string Style { get; set; }
+        public bool IsSigned { get; set; }
+        public string ContactName { get; set; }
+        public string ContactPhoneNumber { get; set; }
+    }
+
+    class Albums
+
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsExplicit { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string BandId { get; set; }
+
+    }
+
+    class Songs
+
+    {
+
+        public int Id { get; set; }
+        public int TrackNumber { get; set; }
+        public string Duration { get; set; }
+        public string AlbumId { get; set; }
+
+    }
+
+    class Program
+
+    {
+        static void DisplayGreeting()
+
         {
 
+            Console.WriteLine("\n\n        Welcome to BeatBOX Studio            \n");
+            Console.WriteLine("    * * * * * * * * * * * * * * * * \n");
+            Console.WriteLine("              Studio Manager        \n");
+            Console.WriteLine("    *-*-*-*-*-*-*-MENU-*-*-*-*-*-*  \n");
 
-
+            Console.WriteLine("MUSIC ADMINISTRATION\n");
             Console.WriteLine("(1.) Add a new band");
-            Console.WriteLine("(2.) View all the bands");
-            Console.WriteLine("(3.) Add an album for a band");
-            Console.WriteLine("(4.) Add a song to an album");
-            Console.WriteLine("(5.) Let a band go"); //(update isSigned to false)
-            Console.WriteLine("(6.) Resign a band"); //(update isSigned to true)
-            Console.WriteLine("(7.) Prompt for a band name and view all their albums");
-            Console.WriteLine("(8.) View all albums ordered by ReleaseDate");
-            Console.WriteLine("(9.) View all bands that are signed");
-            Console.WriteLine("(10.) View all bands that are not signed");
-            Console.WriteLine("(11.) Quit the program");
+            Console.WriteLine("(2.) Add an album for a band");
+            Console.WriteLine("(3.) Add a song to an album");
+            Console.WriteLine("(4.) Un-sign a band"); //(update isSigned to false)
+            Console.WriteLine("(5.) Re-sign a band\n"); //(update isSigned to true)
 
+            Console.WriteLine("REPORTS\n");
+            Console.WriteLine("(6.) View all the bands");
+            Console.WriteLine("(7.) View all band albums");
+            Console.WriteLine("(8.) View all albums by ReleaseDate");
+            Console.WriteLine("(9.) View all signed bands");
+            Console.WriteLine("(10.) View all non-signed bands\n");
+            Console.WriteLine("(11.) Quit\n");
 
+            Console.WriteLine("Please input the number from the menu and press ENTER.\n");
+
+        }
+
+        static string PromptForString(string prompt)
+
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+
+            return userInput;
+
+        }
+
+        static int PromptForInteger(string prompt)
+
+        {
+
+            Console.Write(prompt);
+            int userInput;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+
+            if (isThisGoodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                //Console.WriteLine("This is not a valid entry. Action cancelled.");
+                return 0;
+            }
+        }
+
+        static void Main(string[] args)
+
+        {
+
+            DisplayGreeting();
 
         }
     }
 }
+
--- README.md
+++ README.md
@@ -8,8 +8,8 @@ SQL queries:
 
 Add a new band
 
-INSERT INTO "Band" ("Name", "CountryOfOrigin", "NumberOfMembers", "Website", "Style", "IsSigned", "Contact Name", "ContactPhoneNumber")
-VALUES ('Elton John', 'United States', 1, "eltonjohn.com", "Rock", Yes, "Ricky Martin", 800-321-9000);
+INSERT INTO "Bands" ("Name", "CountryOfOrigin", "NumberOfMembers", "Website", "Style", "IsSigned", "ContactName", "ContactPhoneNumber")
+VALUES ('Elton John', 'United States', 1, 'eltonjohn.com', 'Rock', Yes, 'Ricky Martin', 800-321-9000);
 
 View all the bands
 
@@ -18,33 +18,33 @@ FROM "Bands";
 
 Add an album for a band
 
-INSERT INTO "Album" ("Title", "IsExplicit", "ReleaseDate", "NameId", "SongTitleId") VALUES ("Rocket Man", No, 1980, 1, NULL);
+INSERT INTO "Albums" ("Title", "IsExplicit", "ReleaseDate", "NameId", "SongTitleId") VALUES ("Rocket Man", No, 1980, 1, NULL);
 
 **How do we get and assign FK while adding data? Do we search for it first and add to our query? What if an Album doesn't have a song assigned yet. Populate with NULL?**
 
 Add a song to an album
 
-INSERT INTO "Song" ("TrackNumber", "Title", "Duration") VALUES (4, "Yellow Brick Road", 04:53);
+INSERT INTO "Songs" ("TrackNumber", "Title", "Duration") VALUES (4, "Yellow Brick Road", 04:53);
 
 Let a band go (update isSigned to false)
 
-UPDATE TO "Band"
+UPDATE TO "Bands"
 UPDATE "Bands" SET "IsSigned" = 'False' WHERE "Name" = 'Elton John';
 
 Resign a band (update isSigned to true)
 
-UPDATE TO "Band"
+UPDATE TO "Bands"
 UPDATE "Bands" SET "IsSigned" = 'True' WHERE "Name" = 'Elton John';
 
 Given a band name, view all their albums
 
-SELECT "Band"."Name" = 'Elton John'
-FROM "Band"
-JOIN "Album" ON "BandAlbumSong"."AlbumId" = "Albums"."Id"
+SELECT "Bands"."Name" = 'Elton John'
+FROM "Bands"
+JOIN "Albums" ON "Bands"."AlbumId" = "Albums"."Id"
 
 View all albums (and their associated songs) ordered by ReleaseDate
 
-SELECT "Album."Title","Album".OrderBy"ReleaseDate"
+SELECT "Albums."Title","Albums".OrderBy"ReleaseDate"
 FROM "Albums"
 JOIN "BandAlbumSong" ON "Song"."SongTitleId" = "SongTitle"."Id";
 
