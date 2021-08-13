0dfc736311f2e3dc8861babb5143faa9b2d83a61
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-08-13 10:11:30 -0400

    Menu

--- Program.cs
+++ Program.cs
@@ -2,11 +2,27 @@
 
 namespace RhythmsGonnaGetYou
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("(1.) Add a new band");
+            Console.WriteLine("(2.) View all the bands");
+            Console.WriteLine("(3.) Add an album for a band");
+            Console.WriteLine("(4.) Add a song to an album");
+            Console.WriteLine("(5.) Let a band go"); //(update isSigned to false)
+            Console.WriteLine("(6.) Resign a band"); //(update isSigned to true)
+            Console.WriteLine("(7.) Prompt for a band name and view all their albums");
+            Console.WriteLine("(8.) View all albums ordered by ReleaseDate");
+            Console.WriteLine("(9.) View all bands that are signed");
+            Console.WriteLine("(10.) View all bands that are not signed");
+            Console.WriteLine("(11.) Quit the program");
+
+
+
+        }
     }
-  }
 }
